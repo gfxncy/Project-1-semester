a2d048876a05e6ad26b00718579f04de62850d0c
Pavel Dusman <61388850+blawald@users.noreply.github.com>
2023-11-23 19:46:09 +0300

    хочу сделать голову змеи стрелкой чтобы было видно направление двидения

--- snake_parts.py
+++ snake_parts.py
@@ -6,15 +6,17 @@ WIDTH = 800
 HEIGHT = 600
 
 
-
+#предпологается два класса только под змею: класс головы которая двигается как захочет игрок
+#класс хвоста, который цепляется к голове и повторяет за ней все движения
 class snake_head:
     def __init__(self, screen: pygame.Surface, x, y):
         self.screen = screen
         self.x = x
         self.y = y
+        self.r = 100
         self.min_speed = 10
         self.max_speed = 30
-        self.r = 10
+        self.r_curv = 100
         self.vx = 2
         self.vy = 0
         self.a_rad = 0.2
@@ -22,6 +24,9 @@ class snake_head:
         self.ay = 0
         self.color = (0, 0, 0)
         self.angle = 0
+        self.turning_left = True
+        self.turning_right = False
+        
 
 
     def update_angle(self):
@@ -33,6 +38,13 @@ class snake_head:
             self.angle += math.pi
 
     def move(self):
+        self.update_angle()
+        a_c = (self.vx**2 + self.vy**2)/(self.r_curv)
+        if self.turning_left:
+            self.vx+=a_c*math.cos(self.angle + math.pi/2)
+            self.vy+=a_c*math.sin(self.angle + math.pi/2)
+
+
         if self.x + self.vx < self.r:
             self.x  = WIDTH - self.r
         elif self.x + self.vx > WIDTH - self.r:
@@ -44,16 +56,21 @@ class snake_head:
         else:
             self.x += self.vx
             self.y -= self.vy
-        self.vx+=self.ax
-        self.vy+=self.ay
 
 
     def draw(self):
-        pygame.draw.circle(
-            self.screen,
-            self.color,
-            (self.x, self.y),
-            self.r
+        h = 25
+        w = 50
+        d =  math.sqrt((w/2)**2 + (h/4)**2)
+        a_0 = math.atan(h/(2*w))
+        pygame.draw.polygon(self.screen, self.color, [[self.x + d*math.cos(-1*self.angle+a_0), self.x+d*math.sin(-1*self.angle+a_0)],
+                                                     [self.x + d*math.cos(-1*self.angle-a_0), self.x+d*math.sin(-1*self.angle-a_0)]
+                                                     [self.x - h/4 * math.cos(self.angle+math.pi/2), self.y - h/4 * math.sin(self.angle+math.pi/2)], 
+                                                      [self.x - h/2 * math.cos(self.angle+math.pi/2), self.y - h/2 * math.sin(self.angle+math.pi/2)],
+                                                      [self.x + w/2 * math.cos(self.angle), self.y + w/2 *math.sin(self.angle)],
+                                                      [self.x + h/2 * math.cos(self.angle+math.pi/2), self.y + h/2 * math.sin(self.angle+math.pi/2)],
+                                                      [self.x + h/4 * math.cos(self.angle+math.pi/2), self.y + h/4 * math.sin(self.angle+math.pi/2)]]
+
         )
 
     def boost(self, key):
